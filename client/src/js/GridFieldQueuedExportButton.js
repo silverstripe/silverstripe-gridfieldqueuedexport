65963b5ac327d55d9716d8a0ec8b7252c61e75e2
Guy Sartorelli <36352093+GuySartorelli@users.noreply.github.com>
2023-01-31 13:19:42 +1300

    DEP Upgrade frontend build stack (#71)

---
+++ client/src/js/GridFieldQueuedExportButton.js
@@ -0,0 +1,55 @@
+/* global jQuery, ss */
+(function ($) {
+  // eslint-disable-next-line no-shadow
+  $.entwine('ss', ($) => {
+    $('.gridfield-queued-export__status-pending').entwine({
+
+      onadd() {
+        const self = this;
+        setTimeout(() => { self.checkStatus(); }, 2000);
+      },
+
+      checkStatus() {
+        const url = this.parents('.gridfield-queued-export').attr('data-url');
+        if (!url) return;
+
+        if ($('.gridfield-queued-export__loading').length < 1) {
+          $('.cms-content-header-info').append('<span class="icon font-icon-spinner gridfield-queued-export__loading"></span>');
+        }
+
+        const self = this;
+        jQuery.ajax({
+          headers: { 'X-Pjax': 'CurrentForm,Breadcrumbs' },
+          url,
+          type: 'GET',
+          success(data, status, xhr) {
+            if (!$.contains(document, self[0])) return;
+            $('.cms-container').handleAjaxResponse(data, status, xhr);
+          }
+        });
+      }
+    });
+
+    $('.gridfield-queued-export__status-available').entwine({
+      onadd() {
+        this.checkForDownloadStart();
+      },
+
+      checkForDownloadStart() {
+        const self = this;
+        if (!$.contains(document, self[0])) return;
+
+        const id = this.parents('.gridfield-queued-export').attr('data-id');
+        if (!id) return;
+
+        if (document.cookie.match(new RegExp(`(^|;\\s*)downloaded_${id}\\s*=\\s*true(\\s*;|$)`, 'i'))) {
+          this.replaceWith(
+            `<p>${ss.i18n._t('GridFieldQueuedExportButton.DOWNLOADED', 'Your export has been downloaded.')}</p>`
+          );
+        }
+
+        setTimeout(() => { self.checkForDownloadStart(); }, 500);
+      }
+    });
+  });
+}(jQuery));
